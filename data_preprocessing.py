8f9b1338b5c4f5feff188e7a68c62f1a4209c24b
slslslrhfem <57289103+slslslrhfem@users.noreply.github.com>
2023-06-19 01:07:54 +0900

    데이터 저장 폴더 바꿈

--- data_preprocessing.py
+++ data_preprocessing.py
@@ -21,13 +21,14 @@ from pytorch_lightning.core.lightning import LightningModule
 #from transformers.modeling_bert import BertModel
 from transformers import AdamW
 
-def data_preprocessing():
+def data_preprocessing(to_file):
     
     stocks = fdr.StockListing('KRX')
     ticker_list = list(stocks["Symbol"])
     name_list = list(stocks["Name"])
     Sector_Label = list(stocks["Sector"])
-    end_date = datetime.now() - timedelta(1) # 예를 들어 6/13 새벽 1시에 코드를 돌리면. 6월 12일까지의 뉴스와 주가 정보를 들고옵니다. 그리고 13일의 data를 예측하도록 하면 되겠죠! 
+    end_date = datetime.now() - timedelta(1) # 예를 들어 6/13 새벽 1시에 코드를 돌리면. 6월 12일까지의 뉴스와 주가 정보를 들고옵니다. 그리고 13일의 data를 예측하도록 하면 되겠죠! 6/12 오후 10시에 돌리면 -timedelta(1)을 지우면 됩니다.
+    
     start_date = end_date - timedelta(30) # 일단은 데이터를 1달분량을 들고옵니다.
     end_date = end_date.strftime('%Y%m%d')#'20230531'과 같은식
     start_date = start_date.strftime('%Y%m%d')
@@ -37,8 +38,11 @@ def data_preprocessing():
     trade_data = stock.get_market_ohlcv(start_date, end_date, ticker_list[0])
     adding_list=trade_data.reset_index().values.tolist()
     minlen=len(adding_list)
-    
-    for ticker, name, Sector in tqdm(zip(ticker_list, name_list, Sector_Label)): # 뉴스 크롤링
+
+    pbar = tqdm(zip(ticker_list, name_list, Sector_Label))
+
+
+    for ticker, name, Sector in pbar: # 뉴스 크롤링
         meta_dict = {}
         meta_dict['sector'] = Sector
         meta_dict['name'] = name
@@ -48,12 +52,12 @@ def data_preprocessing():
         os.makedirs(os.path.dirname(filename), exist_ok=True)
         with open(filename,'wb') as fw:
             pickle.dump(meta_dict, fw)
-        #start_crawling(name,Sector)
+        #start_crawling((name,Sector,True))
 
-    print('News를 크롤링 해옵니다')
+    
     with multiprocessing.Pool(5) as p: # 근데 이 코드가 돌다가 Port에러가 뜨기도 합니다.(아마 한 CPU에 Task 2개가 동시에 할당 되면 그런 것 같습니다) 
-        #본 경우에넌 Pool 안에 있는 숫자 5를 줄여보시고, 그래도 안되면 위 loop내의 start_crawling(name,Sector) 주석을 삭제하고 아래 코드 1줄을 지운 뒤 해보시길 바랍니다! 속도차이는 5배 넘게 나는 것 같습니다..
-        r= list(tqdm(p.imap(start_crawling, zip(name_list, Sector_Label)), total=len(name_list)))
+        #본 경우에는 Pool 안에 있는 숫자 5를 줄여보시고, 그래도 안되면 위 loop내의 51번째줄 start_crawling(name,Sector,True) 주석을 삭제하고 아래 코드 1줄을 지운 뒤 해보시길 바랍니다! 속도차이는 5배 넘게 나는 것 같습니다..
+        r= list(tqdm(p.imap(start_crawling, zip(name_list, Sector_Label, [to_file for i in range(len(Sector_Label))])), total=len(name_list)))
         
 
     print("주가를 크롤링 해옵니다.")
@@ -82,3 +86,4 @@ def data_preprocessing():
             continue
     with open('dataset{}/trade_data.pickle'.format(end_date),'wb') as fw:
             pickle.dump(tot_dict, fw)
+    return r, tot_dict
